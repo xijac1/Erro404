86603ebbc90470af2d88f7f368cb472c469c9609
julio amaya <julioamaya127@gmail.com>
2022-10-26 19:26:37 -0500

    new changes

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
+// we will create our project here
+
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-    cout << "hi" << endl;
-    cout << "hello" << endl;
-    cout << "okay" << endl;
+    string name;
+    cout << "Hello please enter your name: ";
+    getline(cin, name);
+    
+    cout << name << " how may i help you?" << endl;
 }
